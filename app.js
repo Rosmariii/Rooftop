9127e26b39d580e163c45bf4daed3fe32e1cf8e4
Rosmari <rosmarifernandez@hotmail.com>
2021-06-13 19:30:23 -0300

    ejercicio pach-delete

--- app.js
+++ app.js
@@ -6,31 +6,137 @@ const path = require('path')
 
 const bodyParser = require('body-parser')
 
+const products = require('./products')
+
+const fs = require('fs')
+const { json } = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 app.get('/', function(req, res){
-let file = path.resolve('src', 'index.html')
-
-    res.sendFile(file)
+    
+        res.json()
 })
 
+app.get('/products', function(req, res){
+        console.log(req.query.page)
 
-app.post('/', function(req, res){
-    res.send(req.body)
+        
+        let results = [...products]
+
+        if (Object.keys(req.query).length > 0) {
+
+            if (req.query.price_min) {
+                results = results.filter(function (product) {
+                    return Number(product.price.replace('$', '')) >= req.query.price_min
+    
+                })
+            }
+            if (req.query.price_max) {
+                results = results.filter(function (product) {
+                    return Number(product.price.replace('$', '')) <= req.query.price_max
+    
+                })
+            }
+
+        } else {
+            results = products.slice(0, 10)
+        }
+        if (req.query.hasOwnProperty('page')) {
+    
+    
+            if(req.query.page) {
+                if (req.query.page > 0) {
+                    start = req.query.page * 10
+                    end = start+10
+                    results = results.slice(start, end)
+                }
+            }
+        }
+
+
+        res.json(results)
 })
 
+app.get('/products/:id', function(req, res){
 
-app.get('/viejo', function(req, res){
-    res.redirect('./nuevo')
+        let product = products.find(function (product) {
+            return product.id == req.params.id
+        }) 
+
+        res.json(product)
 })
 
+app.post('/products', function(req, res){
+    
+    let newProduct = {
+        id : Date.now(),
+        description: '',
+        isVisible: false,
+        ...req.body
+    }
 
-app.get('/nuevo', function(req, res){
-    res.send('esta es la app')
+    let content = fs.readFileSync('./products.json', {encoding:'utf8'})
+    let array = JSON.parse(content)
+    array.push(newProduct)
+    content = JSON.stringify(array)
+    fs.writeFileSync('./products.json', content)
+
+    /*if(true){
+        return res.status(403).json({message: "Cannot post"})
+    }*/
+        res.status(201).json({message: "created", "id": newProduct.id})
+    })
+
+app.patch('/products/:id', function(req, res){
+
+    let productIndex = products.findIndex(item => item.id == req.params.id)
+    let newValues = req.body
+    let newProduct = {
+        ...products[productIndex], ...newValues} 
+    
+    
+    let content = fs.readFileSync('./products.json', {encoding:'utf8'})
+    let array = JSON.parse(content)
+    array[productIndex] = newProduct
+    content = JSON.stringify(array)
+    fs.writeFileSync('./products.json', content)
+
+
+    if(productIndex != -1){
+        res.status(200).json({message:"updated"})
+    } else { 
+    res.status(404).json({message:"Not found"})
+    }
+}) 
+
+app.delete('/products/:id', function(req, res){
+    let isDeleted= null
+    let content = fs.readFileSync('./products.json', {encoding:'utf8'})
+    let array = JSON.parse(content)
+    let newArray = array.filter(item => {
+        if(item.id != req.params.id) {
+            isDeleted = false
+            return true
+        } else {
+            isDeleted = true
+            return false
+        }
+    })
+
+    content = JSON.stringify(newArray)
+    fs.writeFileSync('./products.json', content)
+
+    if (isDeleted) {
+        res.status(200).json({message: "deleted"})
+    } else {
+        res.status(404).json({message: "Not Found"})
+    }
+        
 })
 
-
-app.listen(8000)
+app.listen(8000, () => {
+    console.log(`Example app listening at http://localhost:8000`)
+})
---
+++ data.js
@@ -0,0 +1,44 @@
+let products = [
+    {
+        'id' : 1,
+        'title' : '...',
+        'descripcion' : '...',
+        'price' : 123.98,
+        'stock' : 23,  
+
+    },
+    {
+        'id' : 2,
+        'title' : '...',
+        'descripcion' : '...',
+        'price' : 123.98,
+        'stock' : 23,
+    
+    },
+    {
+        'id' : 3,
+        'title' : '...',
+        'descripcion' : '...',
+        'price' : 123.98,
+        'stock' : 23,
+        
+    },
+    {
+        'id' : 4,
+        'title' : '...',
+        'descripcion' : '...',
+        'price' : 123.98,
+        'stock' : 23,
+        
+    },
+    {
+        'id' : 5,
+        'title' : '...',
+        'descripcion' : '...',
+        'price' : 123.98,
+        'stock' : 23,
+
+    },
+]
+
+module.exports = products
